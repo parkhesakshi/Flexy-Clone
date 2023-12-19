982096bd2f149292433e1b74169847929f2d8de4
Sakshi Parkhe <sakshiparkhe56@gmail.com>
2023-12-20 00:51:26 +0530

    change profile down arrow icon

--- src/app/layout/headernav/headernav.component.js
+++ src/app/layout/headernav/headernav.component.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { IoIosMenu } from "react-icons/io";
 import { CiSearch } from "react-icons/ci";
-import { LuShoppingCart } from "react-icons/lu";
-import { FiMessageSquare } from "react-icons/fi";
+import { PiShoppingCartSimpleThin } from "react-icons/pi";
+import { CiChat1 } from "react-icons/ci";
 import { IoIosNotificationsOutline } from "react-icons/io";
-import { FaAngleDown } from "react-icons/fa6";
+import { GoChevronDown } from "react-icons/go";
 import UIButton from "../../ui/components/button/button.component";
 import { ICON_MD, ICON_SM } from "../../shared/constants/utilities";
 import ProfileImg from "../../../assets/images/profile-img.jpg";
@@ -22,10 +22,10 @@ const headernav = () => {
       </div>
       <div className="headnav-content-right d-flex">
         <UIButton className="icon">
-          <LuShoppingCart size={ICON_MD} />
+          <PiShoppingCartSimpleThin size={ICON_MD} />
         </UIButton>
         <UIButton className="icon">
-          <FiMessageSquare size={ICON_MD} />
+          <CiChat1 size={ICON_MD} />
         </UIButton>
         <UIButton className="icon">
           <IoIosNotificationsOutline size={ICON_MD} />
@@ -33,7 +33,7 @@ const headernav = () => {
         <UIButton className="profile-btn d-flex align-items-center gap-2">
           <img src={ProfileImg} alt="" height={30} width={30} />
           <h6 className="m-0">Hii, Sakshi</h6>
-          <FaAngleDown size={ICON_MD} />
+          <GoChevronDown size={ICON_MD} />
         </UIButton>
       </div>
     </div>
