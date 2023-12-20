bafed0c60760a8efd4abdc831afc362cd1775e99
Abhishek Sonar <abhisonar4121@gmail.com>
2023-12-21 02:09:51 +0530

    router added to app component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
 import React from "react";
 import Layout from "./app/layout/layout.component";
 import "./App.scss";
+import RoutesConfig from "./app/layout/routes/routes.component";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Routes,
+  createRoutesFromElements,
+} from "react-router-dom";
+import Modern from "./app/features/restricted/modern/modern.component";
+import Analytical from "./app/features/restricted/analytical/analitical.component";
+import ECommerce from "./app/features/restricted/eCommerce/eCommerce.component";
 
 const App = () => {
-  const router = createBrowserRouter([
-    {
-      path: "*",
-      Component: RoutesConfig,
-    },
-  ]);
-
-  return (
-    <>
-      <Layout></Layout>
-    </>
+  // const router = createBrowserRouter([
+  //   { path: "/", element: <Layout /> },
+  //   {
+  //     path: "*",
+  //     Component: RoutesConfig,
+  //   },
+  // ]);
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Layout />}>
+        <Route path={"/dashboard/modern"} element={<Modern />} />
+        <Route path={"/dashboard/analytical"} element={<Analytical />} />
+        <Route path={"/dashboard/eCommerce"} element={<ECommerce />} />
+      </Route>
+    )
   );
+
+  return <RouterProvider router={router} />;
 };
 
 export default App;
