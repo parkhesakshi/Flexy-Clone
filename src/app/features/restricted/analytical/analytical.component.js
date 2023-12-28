f7f8dff93867d1b6b265a2bcc1e1974824f8568a
Sakshi Parkhe <sakshiparkhe56@gmail.com>
2023-12-28 21:47:59 +0530

    Analytical feature integreting

--- src/app/features/restricted/analytical/analytical.component.js
+++ src/app/features/restricted/analytical/analytical.component.js
@@ -2,7 +2,16 @@ import React from "react";
 import "./analytical.component.scss";
 
 const Analytical = () => {
-  return <div>Analytical</div>;
+  return (
+    <div className="h-auto">
+      <div className="main-container ">
+        <div className="one modern-cards "></div>
+        <div className="two modern-cards"></div>
+        <div className="three modern-cards"></div>
+        <div className="four modern-cards"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Analytical;
